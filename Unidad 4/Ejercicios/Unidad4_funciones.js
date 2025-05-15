568c5b22dca3bbd0f876c8258184440a88a6f50d
Roberto <robertosarta.rm@gmail.com>
2025-05-15 22:13:19 +0200

    Empiezo ejercicios de funciones, termino hasta el 5 (incluido este ultimo)

---
+++ Unidad 4/Ejercicios/Unidad4_funciones.js
@@ -0,0 +1,53 @@
+/*
+//Ejercicio 1
+function saludoTriple(){
+    console.log(`hola`);
+    console.log(`hola`);
+    console.log(`hola`);
+}
+
+console.log(saludoTriple());
+
+//Ejercicio 2
+function saludar(num){
+    for (let i = 0; i < num; i++) {
+        console.log("hola");
+    }
+}
+console.log(saludar(5));
+console.log(saludar(3));
+
+//Ejercicio 3 
+function sumar(a, b){
+    return a + b;
+}
+console.log(sumar(1, 3));
+
+//Ejercicio 4
+function sumar(a, b){
+    return a + b;
+}
+
+let numero1 = 4
+let numero2 = 4
+
+numero1 = Number(numero1);  //Es innecesario pero por acostumbrarme a poner el "Number" o "parseFloat"
+numero2 = Number(numero2);  //Es innecesario pero por acostumbrarme a poner el "Number" o "parseFloat"
+
+let resultado = sumar(numero1, numero2);
+console.log("el resultado de la suma es:", resultado);
+
+//Ejercicio 5
+let comparacion;
+function primeroMin(a, b){
+    a > b? comparacion = true : comparacion = false;
+}
+primeroMin(8, 2);
+console.log(comparacion);
+
+if (comparacion === true){
+    console.log("El primr numero es mas grande")
+}else {
+    console.log("El primer numero no es mas grande")
+}
+*/
