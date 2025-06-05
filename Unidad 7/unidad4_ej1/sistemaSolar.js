905d3e2d030732eeba22e40f6c06c18fd00e5da0
Roberto <robertosarta.rm@gmail.com>
2025-06-05 21:02:52 +0200

    empiezo con los ejercicios de eventos, ejercicio 1  medias, maquetando y organizando el html, css

---
+++ Unidad 7/unidad4_ej1/sistemaSolar.js
@@ -0,0 +1,63 @@
+let sistemaSolar = {
+  mercurio: {
+    nombre: "mercurio",
+    color: "gris",
+    temperatura: 350,
+    imagen: "https://www.astromia.com/fotosolar/fotos/planetamercurio.jpg",
+  },
+  venus: {
+    nombre: "venus",
+    color: "blanco",
+    temperatura: 460,
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Venus-real_color.jpg/220px-Venus-real_color.jpg",
+  },
+  tierra: {
+    nombre: "tierra",
+    color: "morado",
+    temperatura: 14,
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg",
+  },
+  marte: {
+    nombre: "marte",
+    color: "rojo",
+    temperatura: -46,
+    imagen:
+      "https://discoverexperience.com/wp-content/uploads/2022/11/Marte-1.jpg",
+  },
+  jupiter: {
+    nombre: "jupiter",
+    color: "marrón",
+    temperatura: -121,
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Jupiter_by_Cassini-Huygens.jpg/280px-Jupiter_by_Cassini-Huygens.jpg",
+  },
+  saturno: {
+    nombre: "saturno",
+    color: "amarillo",
+    temperatura: -130,
+    imagen:
+      "https://services.meteored.com/img/article/confirmo-nasa-2025-anillos-saturno-desapareceran-vista-tierra-cassini-equinoccio-1699783037789_512.jpeg",
+  },
+  urano: {
+    nombre: "urano",
+    color: "azul",
+    temperatura: -205,
+    imagen: "http://www.manzanares.es/v2/sites/paseo/img/urano/urano1.jpg",
+  },
+  neptuno: {
+    nombre: "neptuno",
+    color: "azul",
+    temperatura: -220,
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/280px-Neptune_Full.jpg",
+  },
+  pluton: {
+    nombre: "plutón",
+    color: "blanco",
+    temperatura: -229,
+    imagen:
+      "https://static.nationalgeographic.es/files/styles/image_3200/public/03pluto.jpg?w=1900&h=1314",
+  },
+};
