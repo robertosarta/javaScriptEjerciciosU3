339a61b7643f4adcebf48ddac75acfa16a06c648
Roberto <robertosarta.rm@gmail.com>
2025-06-10 00:16:04 +0200

    terminado ejercicio 1 de eventos

--- Unidad 7/unidad4_ej1/sistemaSolar.js
+++ Unidad 7/unidad4_ej1/sistemaSolar.js
@@ -3,7 +3,8 @@ let sistemaSolar = {
     nombre: "mercurio",
     color: "gris",
     temperatura: 350,
-    imagen: "https://www.astromia.com/fotosolar/fotos/planetamercurio.jpg",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Mercury_in_color_-_Prockter07_centered.jpg/330px-Mercury_in_color_-_Prockter07_centered.jpg",
+    colorBoton: "#8b898e",
   },
   venus: {
     nombre: "venus",
@@ -11,6 +12,7 @@ let sistemaSolar = {
     temperatura: 460,
     imagen:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Venus-real_color.jpg/220px-Venus-real_color.jpg",
+    colorBoton: "#dbdad6",  
   },
   tierra: {
     nombre: "tierra",
@@ -18,6 +20,7 @@ let sistemaSolar = {
     temperatura: 14,
     imagen:
       "https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg",
+    colorBoton: "#796d8e",  
   },
   marte: {
     nombre: "marte",
@@ -25,6 +28,7 @@ let sistemaSolar = {
     temperatura: -46,
     imagen:
       "https://discoverexperience.com/wp-content/uploads/2022/11/Marte-1.jpg",
+    colorBoton: "#a25f46",  
   },
   jupiter: {
     nombre: "jupiter",
@@ -32,6 +36,7 @@ let sistemaSolar = {
     temperatura: -121,
     imagen:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Jupiter_by_Cassini-Huygens.jpg/280px-Jupiter_by_Cassini-Huygens.jpg",
+    colorBoton: "#6b6158",  
   },
   saturno: {
     nombre: "saturno",
@@ -39,12 +44,14 @@ let sistemaSolar = {
     temperatura: -130,
     imagen:
       "https://services.meteored.com/img/article/confirmo-nasa-2025-anillos-saturno-desapareceran-vista-tierra-cassini-equinoccio-1699783037789_512.jpeg",
+    colorBoton: "#fcc284",  
   },
   urano: {
     nombre: "urano",
     color: "azul",
     temperatura: -205,
     imagen: "http://www.manzanares.es/v2/sites/paseo/img/urano/urano1.jpg",
+    colorBoton: "#1b6c66",
   },
   neptuno: {
     nombre: "neptuno",
@@ -52,6 +59,7 @@ let sistemaSolar = {
     temperatura: -220,
     imagen:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/280px-Neptune_Full.jpg",
+      colorBoton: "#3147a2",
   },
   pluton: {
     nombre: "plutón",
@@ -59,5 +67,6 @@ let sistemaSolar = {
     temperatura: -229,
     imagen:
       "https://static.nationalgeographic.es/files/styles/image_3200/public/03pluto.jpg?w=1900&h=1314",
+    colorBoton: "#efede1",
   },
 };
---
+++ Unidad 7/unidad4_ej1/unidad4_ej1.js
@@ -0,0 +1,69 @@
+const inputElement = document.querySelector(".search-input");
+const buttonElement = document.querySelector(".search-button");
+const sectionPlanets = document.querySelector(".section-planets");
+const planetasAgregados = [];
+
+function createElement(el, elClass, src, textContent) {
+    const newEl = document.createElement(el);
+    if(elClass) newEl.className = (elClass);  
+    if(src) newEl.src = (src);
+    if(textContent) newEl.textContent = (textContent);
+    return newEl;
+}
+function buscar() {
+    const inputValue = inputElement.value.toLowerCase();
+    const planeta = sistemaSolar[inputValue];
+    const planetaColor = sistemaSolar[inputValue].color;
+    const planetaTemperatura = sistemaSolar[inputValue].temperatura;
+    if(!planeta){
+        alert("Planeta no encontrado!");
+        return;
+    }
+
+    if (planetasAgregados.includes(planeta.nombre.toLowerCase())){
+        alert("Tu planeta ya esta en la lista!");
+        return;
+    }
+    planetasAgregados.push(planeta.nombre.toLowerCase());
+
+    const sectionPlanetsContainer = createElement("div", "section-planets_container");
+    const planetsContainerName = createElement("div", "planets-container_name");
+    const planetName = createElement("h3", null, null, planeta.nombre);
+    sectionPlanets.appendChild(sectionPlanetsContainer);
+    sectionPlanetsContainer.appendChild(planetsContainerName);
+    planetsContainerName.appendChild(planetName);
+
+    const planetsContainerImage = createElement("div", "planets-container_image");
+    const planetsContentImage = createElement("img", "planets-content_image", planeta.imagen);
+    sectionPlanetsContainer.appendChild(planetsContainerImage);
+    planetsContainerImage.appendChild(planetsContentImage);
+
+    const planetsContainerList = createElement("div", "planets-container_list");
+    const uList = createElement("ul");
+    const listElement1 = createElement("li", "list-element", null, `Color: ${planetaColor}`);
+    const listElement2 = createElement("li", "list-element", null, `Temperatura: ${planetaTemperatura}`);
+    sectionPlanetsContainer.appendChild(planetsContainerList);
+    planetsContainerList.appendChild(uList);
+    uList.appendChild(listElement1);
+    uList.appendChild(listElement2);
+
+    const planetsContainerButton = createElement("div", "planets-container_button");
+    const buttonColor = createElement("button", "button-color", null, "Cambiar color");
+    sectionPlanetsContainer.appendChild(planetsContainerButton);
+    planetsContainerButton.appendChild(buttonColor);
+
+    let colorChange = false;
+    buttonColor.addEventListener("click", () => {
+        if (!colorChange){
+            sectionPlanetsContainer.style.backgroundColor = planeta.colorBoton;
+        }else {
+            sectionPlanetsContainer.style.backgroundColor = "#343a40";
+        }
+        colorChange = !colorChange;
+    });
+};
+buttonElement.addEventListener("click", buscar);
+inputElement.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") buscar();
+});
+
