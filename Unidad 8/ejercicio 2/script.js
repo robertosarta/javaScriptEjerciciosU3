5575f8cb0b4a2c5bc7b0768f20342cb49f545f29
Roberto <robertosarta.rm@gmail.com>
2025-08-15 18:14:32 +0200

    Termino ejercicio 2

--- Unidad 8/ejercicio 2/script.js
+++ Unidad 8/ejercicio 2/script.js
@@ -1,11 +1,37 @@
+let datosGlobales = null;
+let charName = document.querySelector(".char-name");
+let charId = document.querySelector(".char-id");
+let charImg = document.querySelector(".char-img");
+let charFilms = document.querySelector(".char-films");
+let charButton = document.querySelector(".char-button");
 async function pedir(){
     try {
-        const numeroAleatorio = () => Math.floor(Math.random() * 500) + 1;
+        const numeroAleatorio = Math.floor(Math.random() * 500) + 1;
         const res = await fetch(`https://api.disneyapi.dev/character/${numeroAleatorio}`);
         const datos = await res.json();
-        datosGlobales = datos;
-        // imprimir();
+
+        // console.log(datos);
+        return datos;
     } catch (error) {
         alert(error);
     }
 };
+
+async function gestionarDatos() {
+    try {
+        const personaje = await pedir();
+        charName.textContent = personaje.data.name;
+        charId.textContent = personaje.data._id;
+        charImg.src = personaje.data.imageUrl;
+        
+        if (personaje.data.films.length === 0) {
+            charFilms.textContent = "No tiene películas registradas";
+        } else {
+            charFilms.textContent = personaje.data.films.join(", ");
+        }
+    } catch (error) {
+        alert("No se ha podido obtener el personaje")
+    }
+}
+
+charButton.addEventListener("click", () => {gestionarDatos();});
